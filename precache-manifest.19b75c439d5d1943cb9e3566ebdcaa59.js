924675bb3b40869d60214d82d2cd722c4e93e6a3
ErikasBykovskis <43473075+ErikasBykovskis@users.noreply.github.com>
2020-09-16 18:10:32 +0300

    Updates

---
+++ precache-manifest.19b75c439d5d1943cb9e3566ebdcaa59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f63a12a00083707c0dea009e5f42d60",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4c940d1ed629361b347a",
+    "url": "/monsters-rolodex/static/css/main.bbf152b3.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c940d1ed629361b347a",
+    "url": "/monsters-rolodex/static/js/main.390d3374.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
